5abc6df844177570701a2dd6a617ccb9d4d8584b
Sergey <aseev-1977@list.ru>
2022-12-23 23:12:05 +0300

    Задача 15

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число дня недели: ");
+int weekNumber = int.Parse(Console.ReadLine());
+if (weekNumber == 6 || weekNumber == 7)
+{
+    Console.Write($"{weekNumber} день недели это ВЫХОДНОЙ");
+}
+else
+{
+    Console.Write($"{weekNumber} день недели это НЕ ВЫХОДНОЙ");
+}
